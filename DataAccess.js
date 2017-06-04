073b846630a96f0a03fdd31f2a56f999db6b2347
mohabat06 <gillm2@seattleu.edu>
2017-06-04 16:09:32 -0700

    checking in code

--- DataAccess.js
+++ DataAccess.js
@@ -17,6 +17,6 @@ var DataAccess = (function () {
     };
     return DataAccess;
 }());
-DataAccess.DB_CONNECTION_STRING = 'mongodb://dbAdmin:test@localhost:3000/recipeSample/?authSource=admin';
+DataAccess.DB_CONNECTION_STRING = 'mongodb://admintest:Amyandtheboyz@ds064799.mlab.com:64799/gainzgoalzdb';
 DataAccess.connect();
 exports["default"] = DataAccess;
