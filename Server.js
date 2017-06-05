088effdac85fde780143232abce65b7903b3da42
mohabat06 <gillm2@seattleu.edu>
2017-06-05 15:34:38 -0700

    Updated server.ts and app.ts

--- Server.js
+++ Server.js
@@ -1,49 +1,47 @@
 "use strict";
-exports.__esModule = true;
-var http = require("http");
-var debug = require("debug");
-var App_1 = require("./App");
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const debug = require("debug");
+const App_1 = require("./App");
 //
-var Server = (function () {
-    function Server() {
-        var _this = this;
+class Server {
+    constructor() {
         debug('ts-express:server');
         this.portValue = this.normalizePort(process.env.PORT || 8080);
-        App_1["default"].set('port', this.portValue);
-        this.server = http.createServer(App_1["default"]);
+        App_1.default.set('port', this.portValue);
+        this.server = http.createServer(App_1.default);
         this.server.listen(this.portValue);
-        this.server.on('error', function (error) {
+        this.server.on('error', (error) => {
             if (error.syscall !== 'listen')
                 throw error;
-            var bind = (typeof _this.port === 'string') ? 'Pipe ' + _this.port : 'Port ' + _this.port;
+            let bind = (typeof this.port === 'string') ? 'Pipe ' + this.port : 'Port ' + this.port;
             switch (error.code) {
                 case 'EACCES':
-                    console.error(bind + " requires elevated privileges");
+                    console.error(`${bind} requires elevated privileges`);
                     process.exit(1);
                     break;
                 case 'EADDRINUSE':
-                    console.error(bind + " is already in use");
+                    console.error(`${bind} is already in use`);
                     process.exit(1);
                     break;
                 default:
                     throw error;
             }
         });
-        this.server.on('listening', function () {
-            var addr = _this.server.address();
-            var bind = (typeof addr === 'string') ? "pipe " + addr : "port " + addr.port;
-            debug("Listening on " + bind);
+        this.server.on('listening', () => {
+            let addr = this.server.address();
+            let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+            debug(`Listening on ${bind}`);
         });
     }
-    Server.prototype.normalizePort = function (val) {
-        var port = (typeof val === 'string') ? parseInt(val, 10) : val;
+    normalizePort(val) {
+        let port = (typeof val === 'string') ? parseInt(val, 10) : val;
         if (isNaN(port))
             return val;
         else if (port >= 0)
             return port;
         else
             return false;
-    };
-    return Server;
-}());
+    }
+}
 new Server();
--- googleOauth2.js
+++ googleOauth2.js
@@ -1,10 +1,7 @@
 "use strict";
-exports.__esModule = true;
-var googleOauth2 = (function () {
-    function googleOauth2() {
-    }
-    return googleOauth2;
-}());
+Object.defineProperty(exports, "__esModule", { value: true });
+class googleOauth2 {
+}
 googleOauth2.id = '167945723113-dqoiot2r7v71nug0ikm00ipc1t7n3eid.apps.googleusercontent.com';
 googleOauth2.secret = 'TR5ExpXFbMfaQZRRCxlbjEIw';
-exports["default"] = googleOauth2;
+exports.default = googleOauth2;
