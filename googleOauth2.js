b0559dfc2b389a08e6f1309965afc84371692ced
mohabat06 <gillm2@seattleu.edu>
2017-06-05 15:24:29 -0700

    Google link

--- googleOauth2.js
+++ googleOauth2.js
@@ -5,6 +5,6 @@ var googleOauth2 = (function () {
     }
     return googleOauth2;
 }());
-googleOauth2.id = 'Your own';
-googleOauth2.secret = 'Your own';
+googleOauth2.id = '167945723113-dqoiot2r7v71nug0ikm00ipc1t7n3eid.apps.googleusercontent.com';
+googleOauth2.secret = 'TR5ExpXFbMfaQZRRCxlbjEIw';
 exports["default"] = googleOauth2;
