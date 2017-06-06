432d33a725ad91ad7549fa90e278e8e2aa7c822f
mohabat06 <gillm2@seattleu.edu>
2017-06-06 13:02:53 -0700

    Changed routes

--- App.ts
+++ App.ts
@@ -100,7 +100,7 @@ class App {
         this.recipes.retrieveSingleRecipe(res, {recipeId: id});
     });
 
-    router.get('/app/recipe/:created_by', (req, res) => {
+    router.get('/app/usersrecipes/:created_by', (req, res) => {
         var createdId = req.params.created_by;
         console.log('Query All recipes by user Id: ' + createdId);
         this.recipes.retrieveUserRecipe(res, {created_by: createdId});
